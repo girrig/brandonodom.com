c072f5d187c379d8273fbe7d971c3882419f45bd
Brandon Odom <girrig3@gmail.com>
2025-04-10 10:34:37 -0500

    Made the portfolio page formatting better on mobile

--- brandonodom.com/app/(root)/portfolio/page.tsx
+++ brandonodom.com/app/(root)/portfolio/page.tsx
@@ -5,12 +5,12 @@ const page = () => {
     <>
       <div className="flex flex-col">
         {/* Header Section */}
-        <div className="w-full pt-6 pb-3 flex justify-center items-center">
+        <div className="w-full pt-6 pb-3 px-4 sm:px-6 flex justify-center items-center">
           <div className="text-center">
             <div className="text-xs tracking-widest uppercase text-gray-500 mb-1">
               Portfolio
             </div>
-            <h1 className="text-3xl font-light mb-6">
+            <h1 className="text-2xl sm:text-3xl font-light mb-4 sm:mb-6">
               Professional and Personal Projects
             </h1>
             <div className="h-px w-16 bg-gray-300 mb-3 mx-auto"></div>
@@ -18,12 +18,12 @@ const page = () => {
         </div>
 
         {/* Projects Panel */}
-        <div className="flex-1 pb-8 px-8 overflow-y-auto">
-          <div className="max-w-4xl mx-auto space-y-8">
+        <div className="flex-1 pb-6 sm:pb-8 px-4 sm:px-8 overflow-y-auto">
+          <div className="max-w-4xl mx-auto space-y-6 sm:space-y-8">
             {/* CodeStar and CodeCatalyst - Image on Left */}
-            <div className="bg-white rounded-lg shadow-xs p-6">
+            <div className="bg-white rounded-lg shadow-xs p-4 sm:p-6">
               <div className="flex flex-col md:flex-row">
-                <div className="w-full md:w-1/2 md:pr-8 mb-6 md:mb-0">
+                <div className="w-full md:w-1/2 md:pr-8 mb-5 md:mb-0">
                   <div className="relative overflow-hidden rounded-sm flex items-center justify-center">
                     <Image
                       src="/portfolio/CodeStarCodeCatalyst.png"
@@ -38,16 +38,16 @@ const page = () => {
                   <h3 className="text-xl font-medium mb-2">
                     CodeStar and CodeCatalyst
                   </h3>
-                  <div className="text-xs tracking-wider uppercase text-gray-500 mb-4">
+                  <div className="text-xs tracking-wider uppercase text-gray-500 mb-3 sm:mb-4">
                     Java / CloudFormation / Templates
                   </div>
-                  <p className="text-sm text-gray-600 mb-4">
+                  <p className="text-sm text-gray-600 mb-3 sm:mb-4">
                     Amazon CodeCatalyst, and previously CodeStar (RIP), is a
                     unified software development service, leveraging the power
                     of the AWS Code Suite, that enables development teams to
                     quickly build and deliver scalable applications using AWS.
                   </p>
-                  <ul className="list-disc text-sm text-gray-600 space-y-1 ml-4 mb-4">
+                  <ul className="list-disc text-sm text-gray-600 space-y-1 ml-4 mb-3 sm:mb-4">
                     <li>
                       Contributed to the release of multiple products and
                       features
@@ -70,10 +70,10 @@ const page = () => {
               </div>
             </div>
 
-            {/* Therabot- Image on Right */}
-            <div className="bg-white rounded-lg shadow-xs p-6">
+            {/* Therabot - Image on Right */}
+            <div className="bg-white rounded-lg shadow-xs p-4 sm:p-6">
               <div className="flex flex-col md:flex-row-reverse">
-                <div className="w-full md:w-1/2 md:pl-8 mb-6 md:mb-0">
+                <div className="w-full md:w-1/2 md:pl-8 mb-5 md:mb-0">
                   <div className="relative overflow-hidden rounded-sm flex items-center justify-center">
                     <Image
                       src="/portfolio/therabot.png"
@@ -86,16 +86,16 @@ const page = () => {
                 </div>
                 <div className="w-full md:w-1/2">
                   <h3 className="text-xl font-medium mb-2">Therabot</h3>
-                  <div className="text-xs tracking-wider uppercase text-gray-500 mb-4">
+                  <div className="text-xs tracking-wider uppercase text-gray-500 mb-3 sm:mb-4">
                     JavaScript / Three.js / UIkit / Autodesk Maya / Robotics
                   </div>
-                  <p className="text-sm text-gray-600 mb-4">
+                  <p className="text-sm text-gray-600 mb-3 sm:mb-4">
                     Therabot is a robotic dog and support companion for
                     individuals undergoing therapy. It exhibits a lifelike
                     awareness of its surroundings, including sensing and
                     responding to touch.
                   </p>
-                  <ul className="list-disc text-sm text-gray-600 space-y-1 ml-4 mb-4">
+                  <ul className="list-disc text-sm text-gray-600 space-y-1 ml-4 mb-3 sm:mb-4">
                     <li>
                       Created a web UI to more easily sequence the robots motors
                     </li>
@@ -115,9 +115,9 @@ const page = () => {
             </div>
 
             {/* ANVEL-ROS - Image on Left */}
-            <div className="bg-white rounded-lg shadow-xs p-6">
+            <div className="bg-white rounded-lg shadow-xs p-4 sm:p-6">
               <div className="flex flex-col md:flex-row">
-                <div className="w-full md:w-1/2 md:pr-8 mb-6 md:mb-0">
+                <div className="w-full md:w-1/2 md:pr-8 mb-5 md:mb-0">
                   <div className="relative overflow-hidden rounded-sm flex items-center justify-center">
                     <Image
                       src="/portfolio/ANVELROS.png"
@@ -130,17 +130,17 @@ const page = () => {
                 </div>
                 <div className="w-full md:w-1/2">
                   <h3 className="text-xl font-medium mb-2">ANVEL-ROS</h3>
-                  <div className="text-xs tracking-wider uppercase text-gray-500 mb-4">
+                  <div className="text-xs tracking-wider uppercase text-gray-500 mb-3 sm:mb-4">
                     C++ / Python / ANVEL / ROS / Robotics
                   </div>
-                  <p className="text-sm text-gray-600 mb-4">
+                  <p className="text-sm text-gray-600 mb-3 sm:mb-4">
                     The Autonomous Navigation Virtual Environment Laboratory
                     (ANVEL) simulator was combined with the Robot Operating
                     System (ROS) to create a new interface, the ANVEL-ROS
                     bridge. It was developed to show the realism of the ANVEL
                     simulator by mimicking the actions of real world robots.
                   </p>
-                  <ul className="list-disc text-sm text-gray-600 space-y-1 ml-4 mb-4">
+                  <ul className="list-disc text-sm text-gray-600 space-y-1 ml-4 mb-3 sm:mb-4">
                     <li>Created the ROS side of the ANVEL-ROS bridge</li>
                     <li>Developed multiple UIs for human factors research</li>
                     <li>
@@ -161,9 +161,9 @@ const page = () => {
             </div>
 
             {/* Llama Game - Image on Right */}
-            <div className="bg-white rounded-lg shadow-xs p-6">
+            <div className="bg-white rounded-lg shadow-xs p-4 sm:p-6">
               <div className="flex flex-col md:flex-row-reverse">
-                <div className="w-full md:w-1/2 md:pl-8 mb-6 md:mb-0">
+                <div className="w-full md:w-1/2 md:pl-8 mb-5 md:mb-0">
                   <div className="relative overflow-hidden rounded-sm flex items-center justify-center">
                     <Image
                       src="/portfolio/llamagame.png"
@@ -176,10 +176,10 @@ const page = () => {
                 </div>
                 <div className="w-full md:w-1/2">
                   <h3 className="text-xl font-medium mb-2">Llama Game</h3>
-                  <div className="text-xs tracking-wider uppercase text-gray-500 mb-4">
+                  <div className="text-xs tracking-wider uppercase text-gray-500 mb-3 sm:mb-4">
                     Python / Pygame
                   </div>
-                  <p className="text-sm text-gray-600 mb-4">
+                  <p className="text-sm text-gray-600 mb-3 sm:mb-4">
                     Llama Game is a personal project I started back in 2012. It
                     was intended to be a fun, small project with simple
                     mechanics and a basic GUI. But it became more of a
@@ -187,7 +187,7 @@ const page = () => {
                     feedback. Also a good friend of mine created some cute
                     graphics to give it some life.
                   </p>
-                  <ul className="list-disc text-sm text-gray-600 space-y-1 ml-4 mb-4">
+                  <ul className="list-disc text-sm text-gray-600 space-y-1 ml-4 mb-3 sm:mb-4">
                     <li>
                       <a
                         href="https://brandonodom.com"
@@ -215,25 +215,25 @@ const page = () => {
             </div>
 
             {/* Houses of Cards - Image on Left */}
-            <div className="bg-white rounded-lg shadow-xs p-6">
+            <div className="bg-white rounded-lg shadow-xs p-4 sm:p-6">
               <div className="flex flex-col md:flex-row">
-                <div className="w-full md:w-1/2 md:pr-8 mb-6 md:mb-0">
+                <div className="w-full md:w-1/2 md:pr-8 mb-5 md:mb-0">
                   <div className="relative overflow-hidden rounded-sm flex items-center justify-center">
                     <Image
                       src="/portfolio/housesofcards.png"
                       alt="Houses of Cards image"
                       width={225}
                       height={315}
-                      className="w-1/2 h-full object-contain"
+                      className="w-1/2 h-full object-contain mx-auto md:mx-0"
                     />
                   </div>
                 </div>
                 <div className="w-full md:w-1/2">
                   <h3 className="text-xl font-medium mb-2">Houses of Cards</h3>
-                  <div className="text-xs tracking-wider uppercase text-gray-500 mb-4">
+                  <div className="text-xs tracking-wider uppercase text-gray-500 mb-3 sm:mb-4">
                     Javascript / Socket.IO
                   </div>
-                  <p className="text-sm text-gray-600 mb-4">
+                  <p className="text-sm text-gray-600 mb-3 sm:mb-4">
                     Houses of Cards is an upcoming ruling class tabletop game
                     that can be played with or without RPG elements. Designed to
                     be simple to understand and quick to set up. Currently
@@ -241,7 +241,7 @@ const page = () => {
                     info and links will be added here when it comes closer to
                     release.
                   </p>
-                  <ul className="list-disc text-sm text-gray-600 space-y-1 ml-4 mb-4">
+                  <ul className="list-disc text-sm text-gray-600 space-y-1 ml-4 mb-3 sm:mb-4">
                     <li>
                       <a
                         href="https://twitter.com/RazgrizTwo"
